7b17cfdf447daf4109101199b850a36786090b99
nasimsarwar <nasimsarwar07@gmail.com>
2021-01-17 12:02:10 +0530

    master commit

---
+++ src/main/java/com/tyremandi/TyremandiBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.tyremandi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TyremandiBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TyremandiBackendApiApplication.class, args);
+	}
+
+}
